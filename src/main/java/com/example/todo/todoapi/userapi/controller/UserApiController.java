56885c44159c07dacd94b6ba28b93759c4404efa
dlwlsgur0909 <dlwlsgur0909@naver.com>
2023-01-26 15:58:35 +0900

    # 이메일 중복 기능

--- src/main/java/com/example/todo/todoapi/userapi/controller/UserApiController.java
+++ src/main/java/com/example/todo/todoapi/userapi/controller/UserApiController.java
@@ -7,18 +7,17 @@ import com.example.todo.todoapi.userapi.exception.NoRegisteredArgumentsException
 import com.example.todo.todoapi.userapi.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
+@CrossOrigin
 public class UserApiController {
 
     private final UserService userService;
@@ -43,18 +42,35 @@ public class UserApiController {
             return ResponseEntity
                     .ok()
                     .body(responseDTO);
+            // 예외 상황 2가지 (dto가 null인 문제, 이메일 중복 문제)
         }catch (NoRegisteredArgumentsException e) {
             log.warn("필수 가입 정보를 다시 확인하세요.");
             return ResponseEntity
                     .badRequest()
                     .body(e.getMessage());
-            // 예외 상황 2가지 (dto가 null인 문제, 이메일 중복 문제)
+
         }catch (DuplicatedEmailException e) {
             log.warn("중복된 이메일 입니다. 다른 이메일을 입력해주세요.");
             return ResponseEntity
                     .badRequest()
                     .body(e.getMessage());
         }
+
+    }
+
+    // 이메일 중복 확인 요청 처리
+    @GetMapping("/check")
+    public ResponseEntity<?> checkEmail(@RequestParam String email) { // @RequestParam 생략 가능
+        if(email == null || email.trim().equals("")) {
+            return ResponseEntity
+                    .badRequest()
+                    .body("이메일을 전달해 주세요!");
+        }
+        boolean flag = userService.isDuplicate(email);
+        log.info("{} 중복 여부?? - {}", email, flag);
+        return ResponseEntity
+                .ok()
+                .body(flag);
 
     }
 
